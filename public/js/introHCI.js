492257fad0c67f22604e1c7ab361e8a06e98e52a
Phong Tran <phongtran0417@gmail.com>
2016-02-11 11:27:04 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,17 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addDeets);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addDeets(result) {
+	console.log(result);
+	var id = result['id'];
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+	 '<p>' + result['summary'] + '</p>' + 
+	 '<p><small>' + result['date'] + '</small></p>';
+	$("#project" + id + " .details").html(projectHTML);
 }
 
 /*
@@ -34,5 +44,17 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
 	console.log("User clicked on color button");
+	$.get("/palette/", retrieveColor);
+}
+
+function retrieveColor(result) {
+	var hex = result['colors'];
+	var colors = hex['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
